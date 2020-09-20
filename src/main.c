e40684977642ad5ad29389b2f8d1965afa722bfc
Айрат <airat.must@yandex.ru>
2020-09-21 00:56:28 +0300

    Fix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: air_must <air_must@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/30 18:55:04 by slynell           #+#    #+#             */
-/*   Updated: 2020/09/21 00:50:13 by air_must         ###   ########.fr       */
+/*   Updated: 2020/09/21 00:56:08 by air_must         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,6 @@ int main(void)
 	lem = lemin_create();
 	lemin_read(ROOMS, lem);
 	apply_algo(lem);
-	print_paths(lem, 0);
 	get_path(lem);
 	distribution_ants(lem);
 	togo_ants(lem);
