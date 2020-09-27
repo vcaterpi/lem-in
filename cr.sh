d2a87c5cdefcab660320c0aad30b754a93f28ec3
Voldemort Caterpie <vcaterpi@mi-u5.msk.21-school.ru>
2020-09-27 14:49:21 +0300

    test_script

--- cr.sh
+++ cr.sh
@@ -1,2 +1,35 @@
 #Компилирует и запускает прогу с тестовыми файлами
-rm *.out; cd libft; make ; cd .. ; gcc src/main/main.c src/params/* src/print/* src/algo/* -Iincl -Llibft -lftprintf; ./a.out
+
+rm graph;
+
+echo ""
+echo "INPUT NUMBER OF TRIALS"
+read TRIALS
+echo ""
+
+FLAGS[0]="--flow-one"
+FLAGS[1]="--flow-ten"
+FLAGS[2]="--flow-thousand"
+FLAGS[3]="--big"
+FLAGS[4]="--big-superposition"
+
+I=0;
+while [ "$I" -lt 5 ]; do
+	OK=0;
+	COUNT=$TRIALS;
+	while [ "$COUNT" -gt 0 ]; do
+		./gen ${FLAGS[$I]} > graph
+		AWAITED_RESULT=$(cat graph | grep -m 1 "Here is the" | awk '{print $8}');
+		REAL_RESULT=$(cat graph | ./lem-in | wc -l | awk '{print $1}');
+		let "DELTA = REAL_RESULT - AWAITED_RESULT";
+		if [[ "$DELTA" -lt  4 ]]; then
+			((OK++));
+		fi
+		((COUNT--));
+	done
+	echo "RAN ${FLAGS[$I]} $TRIALS times"
+	echo "OK $OK times"
+	echo ""
+	((I++));
+done
+
